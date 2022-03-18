ac0eb026f332dfd259a67a2423a56a9e2bdce18d
Roberth <rrondoc@gmail.com>
2022-03-17 19:08:44 -0500

    Update task 4

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,15 +1,13 @@
--- Initial
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity int NOT NULL DEFAULT 10
-);
-CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(255) NOT NULL,
-    number int NOT NULL
-);
-INSERT INTO items (name)
-VALUES ("apple"),
-    ("pineapple"),
-    ("pear");
+-- 4-store.sql / 0x0C. MySQL advanced /  Web Stack programming ― Back-end
+-- Creates a trigger that decreases the quantity of an item after adding a new order
+DELIMITER $$
+CREATE
+    TRIGGER decreases_items_quantity AFTER INSERT ON orders
+    FOR EACH ROW
+        BEGIN
+        UPDATE items
+            SET quantity = quantity - NEW.number
+            WHERE name = NEW.item_name;
+        END;
+$$
+DELIMITER ;
