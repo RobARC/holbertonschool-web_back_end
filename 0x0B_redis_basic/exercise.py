1540702d454b19dc27d47c56f03e3f338a0f6f48
RobARC <rrondonc@gmail.com>
2022-03-19 13:42:49 -0500

    Uploading task 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -22,3 +22,25 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get (self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """ Get data from redis and transform it to its python type """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ Method get_str transform a redis type varible to str python type """
+        Myvar = self._redis.get(key)
+        return Myvar.decode('utf8')
+
+    def get_int(self, key: str) -> int:
+        """ Method get_int transform a redis type varible to int python type """
+        Myvar = self._redis.get(key)
+        try:
+            Myvar = int(Myvar.decode('utf8'))
+        except Exception:
+            Myvar = 0
+        return Myvar
+    
