f52ca9efc4257b5e00caf97fd6c9c606844c2e1a
RobARC <rrondonc@gmail.com>
2022-03-19 13:48:59 -0500

    Updating task 2 pycodestyle

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -23,7 +23,8 @@ class Cache():
         self._redis.set(key, data)
         return key
 
-    def get (self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+    def get(self, key: str, fn: Callable = None) \
+            -> Union[str, bytes, int, float]:
         """ Get data from redis and transform it to its python type """
         data = self._redis.get(key)
         if fn:
@@ -31,16 +32,17 @@ class Cache():
         return data
 
     def get_str(self, key: str) -> str:
-        """ Method get_str transform a redis type varible to str python type """
+        """ Method get_str transform a redis type varible
+            to str python type """
         Myvar = self._redis.get(key)
         return Myvar.decode('utf8')
 
     def get_int(self, key: str) -> int:
-        """ Method get_int transform a redis type varible to int python type """
+        """ Method get_int transform a redis type varible
+             to int python type """
         Myvar = self._redis.get(key)
         try:
             Myvar = int(Myvar.decode('utf8'))
         except Exception:
             Myvar = 0
         return Myvar
-    
