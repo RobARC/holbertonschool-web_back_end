3042fc0909b8aba206698d40b8341ba791a3b19d
RobARC <rrondonc@gmail.com>
2022-03-19 16:36:36 -0500

    Uploading task 5

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Module to obtain the HTML content of a particular URL and returns it."""
+
+from functools import wraps
+import redis
+import requests
+from typing import Callable
+
+client = redis.Redis()
+
+
+def count_request(method: Callable) -> Callable:
+    """ Method decorator to count how many request has been made """
+
+    def wrapper(url):
+        """ function wrapper """
+        client.incr(f"count:{url}")
+        cached_html = client.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode("utf8")
+
+        html = method(url)
+        client.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_request
+def get_page(url: str) -> str:
+    """ Get the html content of a web page """
+    req = requests.get(url)
+    return req.text
