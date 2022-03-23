04f10a7e3cce3a7d30d7fd90ac37861c53e88935
RobARC <rrondonc@gmail.com>
2022-03-22 19:21:44 -0500

    updating task 10

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+##!/usr/bin/env python3
 """ Update topics """
 
 
@@ -6,10 +6,14 @@ from typing import List
 
 
 def update_topics(mongo_collection: object, name: str, topics: List[str]):
-    """function that changes all topics of a school document based on the name"""
-
+    """Change all topics of a school document based on the name
+    Args:
+        mongo_collection (object): [description]
+        name (str): [description]
+        topics (List[str]): [description]
+    """
     data = mongo_collection.update_many(
-                {"name": name}, 
-                {"$set": "topics": topics}}
-                )
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
     return data.modified_count
