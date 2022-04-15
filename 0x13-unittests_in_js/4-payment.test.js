4ce8eb70a0616b80044f6bed5276e84c265a8cdf
RobARC <rrondonc@gmail.com>
2022-04-14 22:27:38 -0500

    Uploading task4

---
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -0,0 +1,21 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
+
+describe('Stubs',  () => {
+  it('has the same math', () => {
+    const stubUtils = sinon.stub(Utils, 'calculateNumber');
+    stubUtils.return(10);
+    const spyConsole = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(stubUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(stubConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+    stubUtils.restore();
+    spyConsole.restore();
+  });
+});
